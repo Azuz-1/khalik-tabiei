ab61f2ca70dd3339c732749488611286e8be4f13
Azuz-1 <artankagrsag@gmail.com>
2026-09-12 03:15:39 +0300

    content: activate 900-prompt bank

--- server/src/game/imitationPrompts.extra.ts
+++ server/src/game/imitationPrompts.extra.ts
@@ -1,3 +1,4 @@
+import { EXPANSION_IMITATION_PROMPTS } from "./imitationPrompts.expansion.js";
 import { EXTRA_HANDS_PROMPTS } from "./imitationPrompts.extra.hands.js";
 import { EXTRA_NUMBER_PROMPTS } from "./imitationPrompts.extra.number.js";
 import { EXTRA_POINT_PROMPTS } from "./imitationPrompts.extra.point.js";
@@ -6,4 +7,5 @@ export const EXTRA_IMITATION_PROMPTS = [
   ...EXTRA_HANDS_PROMPTS,
   ...EXTRA_POINT_PROMPTS,
   ...EXTRA_NUMBER_PROMPTS,
+  ...EXPANSION_IMITATION_PROMPTS,
 ];
